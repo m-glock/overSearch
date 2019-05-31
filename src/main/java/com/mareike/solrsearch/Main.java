a78b2b4256dcc365b5f484fcc786a25fb96f655f
m-glock <s0553536@htw-berlin.de>
2019-05-31 12:49:45 +0200

    authorization working, request not yet

--- src/main/java/com/mareike/solrsearch/Main.java
+++ src/main/java/com/mareike/solrsearch/Main.java
@@ -1,7 +1,16 @@
 package com.mareike.solrsearch;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import com.microsoft.graph.authentication.IAuthenticationProvider;
+import com.microsoft.graph.core.DefaultClientConfig;
+import com.microsoft.graph.core.IClientConfig;
+import com.microsoft.graph.models.extensions.IGraphServiceClient;
+import com.microsoft.graph.requests.extensions.CustomRequestBuilder;
+import com.microsoft.graph.requests.extensions.GraphServiceClient;
+import com.microsoft.graph.requests.extensions.IDriveRequestBuilder;
 import org.apache.solr.client.solrj.SolrServerException;
 
 
@@ -10,9 +19,36 @@ public class Main {
 
     public static void main(String[] args) throws IOException, SolrServerException {
 
-        SolrInstance solr = new SolrInstance("http://localhost:8983/solr", "localDocs");
+        /*SolrInstance solr = new SolrInstance("http://localhost:8983/solr", "localDocs");
         IndexHandler handler = new IndexHandler(solr);
-        handler.indexFiles("C:\\Users\\mareike\\Documents\\Studium\\2.Semester-SS16\\Info2");
+        handler.indexFiles("C:\\Users\\mareike\\Documents\\Studium\\2.Semester-SS16\\Info2");*/
+
+        //authenticate with MS Graph
+        Authenticator authenticator = new Authenticator();
+        IAuthenticationProvider authenticationProvider = authenticator.getAuthenticationProvider();
+
+        StringBuilder result = new StringBuilder();
+        URL url = new URL("https://graph.microsoft.com/v1.0/me/messages");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Authorization", "Bearer " + authenticator.accessToken());
+        conn.setRequestProperty("Accept","application/json");
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+            System.out.println(result.toString());
+        }catch(IOException io){
+            System.out.println(io.getMessage());
+            System.out.println(conn.getResponseMessage());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            System.out.println(conn.getResponseMessage());
+        }
+
 
 
 
