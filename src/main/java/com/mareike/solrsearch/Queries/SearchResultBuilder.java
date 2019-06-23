8bb13cf67ce5304fa605cd9d1a120db6cd43a41e
m-glock <s0553536@htw-berlin.de>
2019-06-23 19:05:23 +0200

    started on formatting of search results

--- src/main/java/com/mareike/solrsearch/Queries/SearchResultBuilder.java
+++ src/main/java/com/mareike/solrsearch/Queries/SearchResultBuilder.java
@@ -3,20 +3,24 @@ package com.mareike.solrsearch.Queries;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SearchResultBuilder {
 
 
     public static String getHTMLForResults(SolrDocumentList list){
         String html = "";
         for (SolrDocument doc : list){
-            String document = "<p>";
-            //TODO: add all display fields here - what to do if field does not exist
-            document += fieldValue(doc, "stream_name");
+            String document = "div>";
+            //TODO: add picture for content type
+            document += "<h4>" + fieldValue(doc, "stream_name") + "</h4>";
             document += fieldValue(doc, "content_type");
             document += fieldValue(doc, "meta_creation_date");
-            //doc.getFieldValue("");
+            document += fieldValue(doc, "owner");
+            document += fieldValue(doc, "path");
 
-            document += "</p>";
+            document += "</div>";
             html += document;
         }
         return html;
@@ -24,22 +28,6 @@ public class SearchResultBuilder {
 
     private static String fieldValue(SolrDocument doc, String fieldName){
         //if the fields exists return the value, otherwise return "unknown"
-        return (doc.getFieldValue(fieldName) == null) ? "unknown" : doc.getFieldValue(fieldName).toString();
-    }
-
-    private class SearchResult{
-
-        String fileName;
-        String path;
-        String contentType;
-        String creator;
-        String dateCreated;
-        String dateModified;
-        int pages;
-
-
-        private SearchResult(){
-
-        }
+        return (doc.getFieldValue(fieldName) == null) ? "unknown" : doc.getFieldValue(fieldName).toString().replace("[]","");
     }
 }
