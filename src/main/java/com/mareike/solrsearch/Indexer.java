2cd452b19872916ebf1578fac78a1c9478b91cfd
m-glock <s0553536@htw-berlin.de>
2019-06-26 16:34:40 +0200

    sending indexing request in new thread and print out while waiting for response

--- src/main/java/com/mareike/solrsearch/Indexer.java
+++ src/main/java/com/mareike/solrsearch/Indexer.java
@@ -1,10 +1,15 @@
 package com.mareike.solrsearch;
 
+import com.mareike.solrsearch.localDirectories.WatchDirectory;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class Indexer {
 
@@ -21,23 +26,36 @@ public class Indexer {
     public static void indexFileOrFolder(String path){
         path = path.replace(" ", "_");
         try{
-            URL url = new URL(functionURL + path);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            final URL url = new URL(functionURL + path);
+            final HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
 
-            int responseCode = con.getResponseCode();
-            //TODO: remove this when everything is working
-            System.out.println("\nSending 'GET' request to URL : " + url);
-            System.out.println("Response Code : " + responseCode);
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run(){
+                    try (BufferedReader in = new BufferedReader(
+                            new InputStreamReader(con.getInputStream()))){
+                        int responseCode = con.getResponseCode();
+                        //TODO: remove this when everything is working
+                        System.out.println("\nSending 'GET' request to URL : " + url);
+                        System.out.println("Response Code : " + responseCode);
 
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+                        String inputLine;
+                        StringBuffer response = new StringBuffer();
+
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+                    }catch(IOException ex){
+                        System.out.println(ex.getMessage());
+                    }
+                }
+            });
+            t.start();
+            while(t.isAlive()){
+                System.out.println(".");
+                TimeUnit.SECONDS.sleep(5);
             }
-            in.close();
         }catch(Exception prot){
             System.out.println("Protocol Exception: " + prot.getClass().toString() + " and message " + prot.getMessage());
         }
