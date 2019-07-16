c087d3b4e980290f1305ce02b5bc2173f8d80fd7
m-glock <s0553536@htw-berlin.de>
2019-07-16 14:47:33 +0200

    fixed directory selection so that child directories are no problem

--- src/main/java/com/mareike/solrsearch/localDirectories/MultiSelectionTree.java
+++ src/main/java/com/mareike/solrsearch/localDirectories/MultiSelectionTree.java
@@ -14,11 +14,33 @@ public class MultiSelectionTree extends JTree {
     public void setSelectionPath(TreePath path) {
 
         System.out.println("MLDebugJTree: setSelectionPath(" + path + ")");
-
+        TreePath[] alreadySelectedPaths = getSelectionModel().getSelectionPaths();
+        String pathToAdd = buildPath(path);
+        for(TreePath tPath : alreadySelectedPaths){
+            String alreadySelected = buildPath(tPath);
+            if(pathToAdd.contains(alreadySelected) && !pathToAdd.equals(alreadySelected)){
+                System.out.println("in if query");
+                JOptionPane.showMessageDialog(null, "This directory is already being indexed together with its parent directory " + buildPath(tPath));
+                return;
+            }else if(alreadySelected.contains(pathToAdd)&& !alreadySelected.equals(pathToAdd)){
+                removeSelectionPath(tPath);
+                System.out.println("removed " + buildPath(tPath) + " from list of selected paths");
+            }
+        }
         addSelectionPath(path);
+        System.out.println(path + " added");
 
         return;
         //super.setSelectionPath(path);
+    }
+
+    private String buildPath(TreePath path){
+        Object[] pathElements = path.getPath();
+        String fullPath = "";
+        for (Object pathElement : pathElements){
+            fullPath = fullPath + pathElement + "/" ;
+        }
+        return fullPath;
     }
 
     @Override
